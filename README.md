a25032e4a37fc90cc9ae7a72623bdf83d1b19665
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-12-21 23:29:42 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# setState in Redux Router Example
+
+# Usage
+
+```sh
+$ npm install -g node-static webpack
+$ webpack
+$ static
+$ open http://localhost:8080/index.html
+```
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Sample
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
