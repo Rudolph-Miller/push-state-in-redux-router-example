af40443ffcb9dc3855c98626ce37c08a3daf7745
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-12-21 23:49:54 +0900

    First impl

--- src/index.js
+++ src/index.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+import { createAction, handleActions } from 'redux-actions';
 
+const INCR_COUNTER = 'INCR_COUNTER';
+const incrCounter = createAction(INCR_COUNTER);
+const DECR_COUNTER = 'DECR_COUNTER';
+const decrCounter = createAction(DECR_COUNTER);
+
+const handleCounter = handleActions({
+  INCR_COUNTER: (counter = 0, action) => {
+    return counter + 1;
+  },
+  DECR_COUNTER: (counter = 0, action) => {
+    return counter - 1;
+  }
+}, 0);
+
+const reducer = combineReducers({
+  counter: handleCounter
+});
+
+@connect(state => {
+  return {
+    counter: state.counter
+  };
+})
 class App extends Component {
   render() {
+    const { dispatch, counter } = this.props;
     return (
       <div>
-        Sample
+        <p>{`COUNTER: ${counter}`}</p>
+        <button onClick={() => { dispatch(incrCounter()); }}>
+          INCREMENT
+        </button>
+        <button onClick={() => { dispatch(decrCounter()); }}>
+          DECREMENT
+        </button>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('app'));
+const store = createStore(reducer);
+
+class Root extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+
+render(<Root />, document.getElementById('app'));
